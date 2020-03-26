21850a92d84a30dce71645df3b77b021f9b7bff2
cdn64 <gitcdn@gmail.com>
2020-03-26 14:01:59 +0100

    Working version

--- src/lib.rs
+++ src/lib.rs
@@ -1,18 +1,41 @@
-// pub trait Token {
-//     fn is_number(&self) -> bool;
-//     fn is_operator(&self) -> bool;
-// }
+const PLUS: i32 = 1;
 
 pub struct Token {
     operator: bool,
     value: i32,
 }
 impl Token {
+    fn from(expression: &str) -> Token {
+        if expression == "+" {
+            Token {
+                operator: true,
+                value: PLUS,
+            }
+        } else {
+            Token {
+                operator: false,
+                value: expression.parse::<i32>().expect("Unparseable number"),
+            }
+        }
+    }
     fn is_number(&self) -> bool {
         self.operator == false
     }
     fn is_operator(&self) -> bool {
         self.operator == true
+    }
+    fn result(&self, a: Token, b: Token) -> Token {
+        if a.is_operator() || b.is_operator() {
+            panic!("There are operators on the stack")
+        }
+        let result = match self.value {
+            PLUS => a.value + b.value,
+            _ => panic!("Operator not supported"),
+        };
+        Token {
+            operator: false,
+            value: result,
+        }
     }
 }
 
@@ -25,12 +48,25 @@ impl Stack {
             stack: Vec::<Token>::new(),
         }
     }
+    pub fn push(&mut self, token: Token) {
+        if token.is_number() {
+            self.stack.push(token)
+        } else {
+            // Apply change
+            if self.stack.len() < 2 {
+                panic!("Operator needs more two or more tokens on the stack");
+            } else {
+                let a = self.stack.pop().unwrap();
+                let b = self.stack.pop().unwrap();
+                self.stack.push(token.result(a, b));
+            }
+        }
+    }
     pub fn parse(&mut self, expression: &str) {
-        println!("{}", expression);
-        self.stack.push(Token {
-            operator: false,
-            value: 4,
-        });
+        let string_parts: Vec<&str> = expression.split(" ").collect();
+        for string_part in &string_parts {
+            self.push(Token::from(string_part));
+        }
     }
     pub fn value(self) -> Result<i32, ()> {
         if self.stack.len() > 0 {
@@ -48,7 +84,7 @@ mod tests {
     #[test]
     fn it_works() {
         let mut stack = Stack::new();
-        stack.parse("2 2 +");
-        assert_eq!(stack.value().unwrap(), 4);
+        stack.parse("1 2 +");
+        assert_eq!(stack.value().unwrap(), 3);
     }
 }
