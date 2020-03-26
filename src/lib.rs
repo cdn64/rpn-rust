4cf6f9dd4c0760c8b4ff872334a2147088c6995f
cdn64 <gitcdn@gmail.com>
2020-03-26 14:19:44 +0100

    Additional example

--- src/lib.rs
+++ src/lib.rs
@@ -21,4 +21,12 @@ mod tests {
         stack.parse("1 2 3 4 5 + - * +");
         assert_eq!(stack.value().unwrap(), -11);
     }
+
+    #[test]
+    fn parsing_multiple_times() {
+        let mut stack = Stack::new();
+        stack.parse("1");
+        stack.parse("2 *");
+        assert_eq!(stack.value().unwrap(), 2);
+    }
 }
